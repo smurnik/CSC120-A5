0d20b8d6f155f16d550385beceb38dfc21e30030
smurnik <95314682+smurnik@users.noreply.github.com>
2022-10-21 20:13:02 -0400

    Update Banner.java

--- Banner.java
+++ Banner.java
@@ -18,8 +18,30 @@ public class Banner {
      * TODO: Modify this method to print a decorative banner, resized to fit the message
      */
     public void display() {
-        System.out.println(this.message);
-    }
+
+        int size = message.length(); //takes length of message
+
+        for (int i=0; i< size + 12; i++) { //creates loop for decoration
+            System.out.print("*");
+        }
+
+        System.out.println("\n" + "*** ~ " + message + " ~ ***"); //prints message with decoration
+
+        for (int i=0; i< size + 12; i++) { //creates loop for decoration
+            System.out.print("*");
+        }
+
+        System.out.println(" "); //decoration
+        for (int i=0; i< size/.667; i++) {
+            System.out.print(" ");
+        }
+        //System.out.println(" ");
+        for (int i=0; i< size/2; i++) {
+            System.out.print("*");
+        }
+
+    }  
+    
 
     /* main method (for testing) */
     public static void main(String[] args) {
