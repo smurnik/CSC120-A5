145dc69a26a1ce12d3cc11c63ac9b126ab9fc3e5
smurnik <95314682+smurnik@users.noreply.github.com>
2022-10-21 20:16:39 -0400

    Update Scarecrow.java

--- Scarecrow.java
+++ Scarecrow.java
@@ -11,19 +11,29 @@ class Scarecrow {
      * TODO: Uncomment remaining parts as you implement each class
      */
     private Pumpkin head;
-    // private Shirt body;
-    // private Pants legs;
+     private Shirt body;
+     private Pants legs;
     // private Hat hat;
-    // private Boot leftFoot;
-    // private Boot rightFoot;
-    // private Banner sign;
+     private Boot leftFoot;
+     private Boot rightFoot;
+     private Banner sign;
     private String message;
 
     /* Constructor
      * TODO: initialize remaining parts
      */
-    public Scarecrow(Pumpkin h) {
+
+
+    public Scarecrow(Pumpkin h, Shirt shirt, Pants pants, Boot lfoot, Boot rfoot, Banner banner, String string) {
         head = h;
+        body = shirt;
+        legs = pants;
+        leftFoot = lfoot;
+        rightFoot = rfoot;
+        sign = banner;
+        message = string;
+        
+
     }
 
     /* Displays the Scarecrow 
@@ -31,19 +41,27 @@ class Scarecrow {
      *       ...in the right order!
     */
     public void display() {
+        sign.display();
         head.display();
+        body.display();
+        legs.display();
+        leftFoot.display();
+        rightFoot.display();
     }
 
     /* Main method (for testing) */
     public static void main(String[] args) {
 
         // TODO: Don't forget to update the line below if you modify the constructor
-        Scarecrow myScarecrow = new Scarecrow(new Pumpkin());
+        Scarecrow myScarecrow = new Scarecrow(new Pumpkin(), new Shirt(), new Pants(), new Boot("Left"), new Boot("Right"), new Banner(""), new String());
 
         // If a message was passed in on the command line, extract and store it
         // TODO: in Step 4, you'll pass this value along to your Banner constructor
         if (args.length > 0) {
             myScarecrow.message = args[0];
+            myScarecrow.sign = new Banner(myScarecrow.message);
+        } else {
+            myScarecrow.message = "Happy Halloween";
         }
 
         myScarecrow.display();
