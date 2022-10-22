9754ff8991f8dfecb813ea44f5998ef68a8326a1
smurnik <95314682+smurnik@users.noreply.github.com>
2022-10-21 20:15:31 -0400

    Update Pants.java

--- Pants.java
+++ Pants.java
@@ -9,8 +9,14 @@ public class Pants {
     /*
      * TODO: Modify this method to print ASCII Art Pants
      */
-    public void display() {
-        System.out.println("Pants?);
+    public void display() { //creates pants
+        System.out.println("           |        |");
+        System.out.println("           |   --   |");
+        System.out.println("           |   ||   |");
+        System.out.println("           |   ||   |");
+        System.out.println("           |   ||   |");
+        System.out.println("           |   ||   |");
+        System.out.println("           |___||___| ");
     }
 
     /* main method (for testing) */
